b0c1f8bb205d95408850569591c78eec4e3ff06e
Patrick Haußmann <36867676+PatrickHaussmann@users.noreply.github.com>
2019-09-02 23:19:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,32 @@ document.getElementById("background").src=imagesrc;
 var now;
 
 function updateTime(){
-  now = new Date().toTimeString().slice(0,8);
+  now = new Date().toTimeString().slice(0,5);
   document.getElementsByClassName("time")[0].innerText = now;
 }
 
 updateTime();
-setInterval(updateTime, 1000);
+
+
+
+hour = Number(new Date().toTimeString().slice(0,2));
+
+var greeting;
+
+if (hour >= 5 && hour < 12) {
+  greeting = "Good morning";
+} else if (hour >= 12 && hour < 18) {
+  greeting = "Good afternoon";
+} else if (hour >= 18 && hour < 21) {
+  greeting = "Good evening";
+} else if (hour >= 21 || hour < 5) {
+  greeting = "Good night";
+} else {
+  greeting = "Good day";
+}
+
+
+document.getElementsByClassName("greeting")[0].innerText = greeting;
 
 
 var quoteurl = "https://quotes.rest/qod.json?category=students"
@@ -27,3 +47,6 @@ fetch(quoteurl).then(function(response) {
 }).catch(function() {
   console.log("no quote for today");
 });
+
+
+setInterval(updateTime, 1000);
